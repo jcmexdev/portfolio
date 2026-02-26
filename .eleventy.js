d464572428b9c628c11d3f91da354b59f04885c0
Juan Carlos Garcia Esquivel <carlos.garcia.esquivel@live.com.mx>
2026-02-25 22:57:07 -0600

    fix(ssg): add missing project assets to src and configure eleventy passthrough for blog routes

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style.css");
   eleventyConfig.addPassthroughCopy("src/cv.css");
   eleventyConfig.addPassthroughCopy("src/script.js");
+  eleventyConfig.addPassthroughCopy("src/locales");
+  eleventyConfig.addPassthroughCopy("src/code-styles.css");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
 
   // Keep the same directory structure internally
   return {
